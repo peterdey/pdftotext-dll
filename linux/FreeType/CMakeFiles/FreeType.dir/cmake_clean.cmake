fd9fa1dfae413af55aafc5d0b740bd7aa5e002c7
Peter Dey <github@realmtech.net>
2024-11-27 21:48:20 +0100

    Removing build artefacts

--- linux/FreeType/CMakeFiles/FreeType.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libFreeType.a"
-  "libFreeType.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/FreeType.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- linux/LibPng/CMakeFiles/LibPng.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libLibPng.a"
-  "libLibPng.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/LibPng.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
